e1251c15889f1b4f019b4aaaded9676c974f35c7
Gagandeep Samra <gsamra890@github.com>
2021-04-22 20:04:49 -0400

    bar chart

--- public/script.js
+++ public/script.js
@@ -1,13 +1,44 @@
+// Database
+    async function windowActions() {
+        const cityRequest = await fetch('/api/city')
+        const cityData = await cityRequest.json();
+       
+        var chart = new CanvasJS.Chart("chartContainer", {
+            animationEnabled: true,
+            theme: "light2", // "light1", "light2", "dark1", "dark2"
+            title:{
+                text: "Top 5 City"
+            },
+            axisX:{
+                axisX: "City"
+            },
+            axisY: {
+                title: "Types of Pollution"
+            },
+            data: [{        
+                type: "column",  
+                showInLegend: true, 
+                legendMarkerColor: "grey",
+                name: "City Name",
+                dataPoints: nameData
+            },
+            {        
+                type: "column",  
+                showInLegend: true, 
+                legendMarkerColor: "red",
+                name: "Pollution",
+                dataPoints: pollutionData
+            },
+                          
+                    
+                ]
+        });
+        cityData.forEach((type) =>{
+            nameData.push({label: type.city_name, y: type.O3});
+            pollutionData.push({label: type.city_name, y: type.O3});
 
-
-
-// Database 
-var map = L.map('map').setView([37.8, -96], 4);
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={pk.eyJ1IjoiZ3NhbXJhIiwiYSI6ImNrbnBsbWUyNDFneXoydnB1YnEyODRoenIifQ.jy9pfKpWoMpdCnyHBXOvPQ}', {
-    attribution: '<a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1,
-}).addTo(map);
-
+        });
+        chart.render();
+        }
+      
+      window.onload = windowActions;
